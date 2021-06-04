7cdac4e53f6c22038ed480318077f37cef654809
Lujufeng <64396553+Lujufeng@users.noreply.github.com>
2021-06-04 07:13:59 -0500

    Add files via upload

---
+++ feature_selector.py
@@ -0,0 +1,388 @@
+
+import pandas as pd
+import numpy as np
+
+import lightgbm as lgb
+
+
+from sklearn.model_selection import train_test_split
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import gc
+
+
+from itertools import chain
+
+class FeatureSelector():
+    
+    def __init__(self, data, labels=None):
+        
+        
+        self.data = data
+        self.labels = labels
+
+        if labels is None:
+            print('No labels provided. Feature importance based methods are not available.')
+        
+        self.base_features = list(data.columns)
+        self.one_hot_features = None
+        
+       
+        self.record_missing = None
+        self.record_single_unique = None
+        self.record_collinear = None
+        self.record_zero_importance = None
+        self.record_low_importance = None
+        
+        self.missing_stats = None
+        self.unique_stats = None
+        self.corr_matrix = None
+        self.feature_importances = None
+        
+       
+        self.ops = {}
+        
+        self.one_hot_correlated = False
+        
+    def identify_missing(self, missing_threshold):
+        
+        self.missing_threshold = missing_threshold
+
+        
+        missing_series = self.data.isnull().sum() / self.data.shape[0]
+        self.missing_stats = pd.DataFrame(missing_series).rename(columns = {'index': 'feature', 0: 'missing_fraction'})
+
+      
+        self.missing_stats = self.missing_stats.sort_values('missing_fraction', ascending = False)
+
+        
+        record_missing = pd.DataFrame(missing_series[missing_series > missing_threshold]).reset_index().rename(columns = 
+                                                                                                               {'index': 'feature', 
+                                                                                                                0: 'missing_fraction'})
+
+        to_drop = list(record_missing['feature'])
+
+        self.record_missing = record_missing
+        self.ops['missing'] = to_drop
+        
+        print('%d features with greater than %0.2f missing values.\n' % (len(self.ops['missing']), self.missing_threshold))
+        
+    def identify_single_unique(self):
+        """Finds features with only a single unique value. NaNs do not count as a unique value. """
+
+        
+        unique_counts = self.data.nunique()
+        self.unique_stats = pd.DataFrame(unique_counts).rename(columns = {'index': 'feature', 0: 'nunique'})
+        self.unique_stats = self.unique_stats.sort_values('nunique', ascending = True)
+        
+        # Find the columns with only one unique count
+        record_single_unique = pd.DataFrame(unique_counts[unique_counts == 1]).reset_index().rename(columns = {'index': 'feature', 
+                                                                                                                0: 'nunique'})
+
+        to_drop = list(record_single_unique['feature'])
+    
+        self.record_single_unique = record_single_unique
+        self.ops['single_unique'] = to_drop
+        
+        print('%d features with a single unique value.\n' % len(self.ops['single_unique']))
+    
+    def identify_collinear(self, correlation_threshold, one_hot=False):
+        
+        self.correlation_threshold = correlation_threshold
+        self.one_hot_correlated = one_hot
+        
+        
+        if one_hot:   
+            features = pd.get_dummies(self.data)
+            self.one_hot_features = [column for column in features.columns if column not in self.base_features]
+            self.data_all = pd.concat([features[self.one_hot_features], self.data], axis = 1)
+            corr_matrix = pd.get_dummies(features).corr()
+
+        else:
+            corr_matrix = self.data.corr()
+        
+        self.corr_matrix = corr_matrix
+        upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k = 1).astype(np.bool))
+        
+        to_drop = [column for column in upper.columns if any(upper[column].abs() > correlation_threshold)]
+        record_collinear = pd.DataFrame(columns = ['drop_feature', 'corr_feature', 'corr_value'])
+        for column in to_drop:
+            corr_features = list(upper.index[upper[column].abs() > correlation_threshold])           
+            corr_values = list(upper[column][upper[column].abs() > correlation_threshold])
+            drop_features = [column for _ in range(len(corr_features))]    
+
+            
+            temp_df = pd.DataFrame.from_dict({'drop_feature': drop_features,
+                                             'corr_feature': corr_features,
+                                             'corr_value': corr_values})
+            record_collinear = record_collinear.append(temp_df, ignore_index = True)
+
+        self.record_collinear = record_collinear
+        self.ops['collinear'] = to_drop
+        
+        print('%d features with a correlation magnitude greater than %0.2f.\n' % (len(self.ops['collinear']), self.correlation_threshold))
+
+    def identify_zero_importance(self, task, eval_metric=None, 
+                                 n_iterations=10, early_stopping = True):
+        
+
+        if early_stopping and eval_metric is None:
+            raise ValueError("""eval metric must be provided with early stopping. Examples include "auc" for classification or
+                             "l2" for regression.""")
+            
+        if self.labels is None:
+            raise ValueError("No training labels provided.")
+        
+        # 独热编码
+        features = pd.get_dummies(self.data)
+        self.one_hot_features = [column for column in features.columns if column not in self.base_features]
+        self.data_all = pd.concat([features[self.one_hot_features], self.data], axis = 1)
+        feature_names = list(features.columns)
+        features = np.array(features)
+        labels = np.array(self.labels).reshape((-1, ))
+        
+        feature_importance_values = np.zeros(len(feature_names))
+        
+        print('Training Gradient Boosting Model\n')
+        
+      
+        for _ in range(n_iterations):
+
+            if task == 'classification':
+                model = lgb.LGBMClassifier(n_estimators=1000, learning_rate = 0.05, verbose = -1)
+
+            elif task == 'regression':
+                model = lgb.LGBMRegressor(n_estimators=1000, learning_rate = 0.05, verbose = -1)
+
+            else:
+                raise ValueError('Task must be either "classification" or "regression"')
+            if early_stopping:
+                
+                train_features, valid_features, train_labels, valid_labels = train_test_split(features, labels, test_size = 0.15, stratify=labels)
+                model.fit(train_features, train_labels, eval_metric = eval_metric,
+                          eval_set = [(valid_features, valid_labels)],
+                          early_stopping_rounds = 100, verbose = -1)
+                
+               
+                gc.enable()
+                del [train_features, train_labels, valid_features, valid_labels]
+                gc.collect()
+                
+            else:
+                model.fit(features, labels)
+
+            
+            feature_importance_values += model.feature_importances_ / n_iterations
+
+        feature_importances = pd.DataFrame({'feature': feature_names, 'importance': feature_importance_values})
+        feature_importances = feature_importances.sort_values('importance', ascending = False).reset_index(drop = True)
+
+        
+        feature_importances['normalized_importance'] = feature_importances['importance'] / feature_importances['importance'].sum()
+        feature_importances['cumulative_importance'] = np.cumsum(feature_importances['normalized_importance'])
+        record_zero_importance = feature_importances[feature_importances['importance'] == 0.0]
+        
+        to_drop = list(record_zero_importance['feature'])
+
+        self.feature_importances = feature_importances
+        self.record_zero_importance = record_zero_importance
+        self.ops['zero_importance'] = to_drop
+        
+        print('\n%d features with zero importance after one-hot encoding.\n' % len(self.ops['zero_importance']))
+    
+    def identify_low_importance(self, cumulative_importance):
+        self.cumulative_importance = cumulative_importance
+        if self.feature_importances is None:
+            raise NotImplementedError("""Feature importances have not yet been determined. 
+                                         Call the `identify_zero_importance` method first.""")
+        self.feature_importances = self.feature_importances.sort_values('cumulative_importance')
+        record_low_importance = self.feature_importances[self.feature_importances['cumulative_importance'] > cumulative_importance]
+
+        to_drop = list(record_low_importance['feature'])
+
+        self.record_low_importance = record_low_importance
+        self.ops['low_importance'] = to_drop
+    
+        print('%d features required for cumulative importance of %0.2f after one hot encoding.' % (len(self.feature_importances) -
+                                                                            len(self.record_low_importance), self.cumulative_importance))
+        print('%d features do not contribute to cumulative importance of %0.2f.\n' % (len(self.ops['low_importance']),
+                                                                                               self.cumulative_importance))
+        
+    def identify_all(self, selection_params):
+        for param in ['missing_threshold', 'correlation_threshold', 'eval_metric', 'task', 'cumulative_importance']:
+            if param not in selection_params.keys():
+                raise ValueError('%s is a required parameter for this method.' % param)
+        
+        
+        self.identify_missing(selection_params['missing_threshold'])
+        self.identify_single_unique()
+        self.identify_collinear(selection_params['correlation_threshold'])
+        self.identify_zero_importance(task = selection_params['task'], eval_metric = selection_params['eval_metric'])
+        self.identify_low_importance(selection_params['cumulative_importance'])
+        
+        
+        self.all_identified = set(list(chain(*list(self.ops.values()))))
+        self.n_identified = len(self.all_identified)
+        
+        print('%d total features out of %d identified for removal after one-hot encoding.\n' % (self.n_identified, 
+                                                                                                  self.data_all.shape[1]))
+    def check_removal(self, keep_one_hot=True):
+        self.all_identified = set(list(chain(*list(self.ops.values()))))
+        print('Total of %d features identified for removal' % len(self.all_identified))
+        
+        if not keep_one_hot:
+            if self.one_hot_features is None:
+                print('Data has not been one-hot encoded')
+            else:
+                one_hot_to_remove = [x for x in self.one_hot_features if x not in self.all_identified]
+                print('%d additional one-hot features can be removed' % len(one_hot_to_remove))
+        
+        return list(self.all_identified)
+    def remove(self, methods, keep_one_hot = True):
+        features_to_drop = []
+      
+        if methods == 'all':
+            data = self.data_all
+                                          
+            print('{} methods have been run\n'.format(list(self.ops.keys())))
+            features_to_drop = set(list(chain(*list(self.ops.values()))))
+            
+        else:
+           
+            if 'zero_importance' in methods or 'low_importance' in methods or self.one_hot_correlated:
+                data = self.data_all
+                
+            else:
+                data = self.data
+            for method in methods:               
+                if method not in self.ops.keys():
+                    raise NotImplementedError('%s method has not been run' % method)
+                    
+                else:
+                    features_to_drop.append(self.ops[method])
+        
+            
+            features_to_drop = set(list(chain(*features_to_drop)))
+            
+        features_to_drop = list(features_to_drop)
+            
+        if not keep_one_hot:
+            
+            if self.one_hot_features is None:
+                print('Data has not been one-hot encoded')
+            else:
+                             
+                features_to_drop = list(set(features_to_drop) | set(self.one_hot_features))
+       
+        
+        data = data.drop(columns = features_to_drop)
+        self.removed_features = features_to_drop
+        
+        if not keep_one_hot:
+        	print('Removed %d features including one-hot features.' % len(features_to_drop))
+        else:
+        	print('Removed %d features.' % len(features_to_drop))
+        
+        return data
+    
+    def plot_missing(self):
+        
+        if self.record_missing is None:
+            raise NotImplementedError("Missing values have not been calculated. Run `identify_missing`")
+        
+        self.reset_plot()
+        plt.style.use('seaborn-white')
+        plt.figure(figsize = (6, 4))
+        plt.hist(self.missing_stats['missing_fraction'], bins = np.linspace(0, 1, 11), edgecolor = 'k', linewidth = 1.5)
+        plt.xticks(np.linspace(0, 1, 11))
+        plt.xlabel('Missing Fraction', size = 14); plt.ylabel('Count of Features', size = 14)
+        plt.title("Fraction of Missing Values Histogram", size = 16)
+        
+    
+    def plot_unique(self):
+        
+        if self.record_single_unique is None:
+            raise NotImplementedError('Unique values have not been calculated. Run `identify_single_unique`')
+        
+        self.reset_plot()
+
+        self.unique_stats.plot.hist(edgecolor = 'k', figsize = (7, 5))
+        plt.ylabel('Frequency', size = 14); plt.xlabel('Unique Values', size = 14)
+        plt.title('Number of Unique Values Histogram', size = 16)
+        
+    
+    def plot_collinear(self, plot_all = False):
+        
+        if self.record_collinear is None:
+            raise NotImplementedError('Collinear features have not been idenfitied. Run `identify_collinear`.')
+        
+        if plot_all:
+        	corr_matrix_plot = self.corr_matrix
+        	title = 'All Correlations'
+        
+        else:
+	        corr_matrix_plot = self.corr_matrix.loc[list(set(self.record_collinear['corr_feature'])), 
+	                                                list(set(self.record_collinear['drop_feature']))]
+
+	        title = "Correlations Above Threshold"
+        f, ax = plt.subplots(figsize=(10, 8))
+        cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+       
+        sns.heatmap(corr_matrix_plot, cmap=cmap, center=0,
+                    linewidths=.25, cbar_kws={"shrink": 0.6})
+
+        
+        ax.set_yticks([x + 0.5 for x in list(range(corr_matrix_plot.shape[0]))])
+        ax.set_yticklabels(list(corr_matrix_plot.index), size = int(160 / corr_matrix_plot.shape[0]))
+
+        
+        ax.set_xticks([x + 0.5 for x in list(range(corr_matrix_plot.shape[1]))])
+        ax.set_xticklabels(list(corr_matrix_plot.columns), size = int(160 / corr_matrix_plot.shape[1]))
+        plt.title(title, size = 14)
+        
+    def plot_feature_importances(self, plot_n = 15, threshold = None):
+        
+        if self.record_zero_importance is None:
+            raise NotImplementedError('Feature importances have not been determined. Run `idenfity_zero_importance`')
+            
+        
+        if plot_n > self.feature_importances.shape[0]:
+            plot_n = self.feature_importances.shape[0] - 1
+
+        self.reset_plot()
+        
+       
+        plt.figure(figsize = (10, 6))
+        ax = plt.subplot()
+
+        ax.barh(list(reversed(list(self.feature_importances.index[:plot_n]))), 
+                self.feature_importances['normalized_importance'][:plot_n], 
+                align = 'center', edgecolor = 'k')
+
+       
+        ax.set_yticks(list(reversed(list(self.feature_importances.index[:plot_n]))))
+        ax.set_yticklabels(self.feature_importances['feature'][:plot_n], size = 12)
+
+        
+        plt.xlabel('Normalized Importance', size = 16); plt.title('Feature Importances', size = 18)
+        plt.show()
+
+       
+        plt.figure(figsize = (6, 4))
+        plt.plot(list(range(1, len(self.feature_importances) + 1)), self.feature_importances['cumulative_importance'], 'r-')
+        plt.xlabel('Number of Features', size = 14); plt.ylabel('Cumulative Importance', size = 14) 
+        plt.title('Cumulative Feature Importance', size = 16)
+
+        if threshold:
+
+            importance_index = np.min(np.where(self.feature_importances['cumulative_importance'] > threshold))
+            plt.vlines(x = importance_index + 1, ymin = 0, ymax = 1, linestyles='--', colors = 'blue')
+            plt.show()
+
+            print('%d features required for %0.2f of cumulative importance' % (importance_index + 1, threshold))
+
+    def reset_plot(self):
+        plt.rcParams = plt.rcParamsDefault
